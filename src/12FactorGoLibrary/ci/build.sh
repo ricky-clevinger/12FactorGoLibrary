0f7edb3129e1979a680fc564ab594407aa636705
Crystal Cox <coxcs1@etsu.edu>
2017-07-31 11:10:24 -0400

    Update build.sh

--- src/12FactorGoLibrary/ci/build.sh
+++ src/12FactorGoLibrary/ci/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e -u -x
-cd ..
+cd ../..
+ls -a
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH
 #export LIBRARY="cgidevlib:Password1@tcp(cgiprojdevlibrary.cxyeb3wmov3g.us-east-1.rds.amazonaws.com:9871)/cgiprojdevlibrary"
