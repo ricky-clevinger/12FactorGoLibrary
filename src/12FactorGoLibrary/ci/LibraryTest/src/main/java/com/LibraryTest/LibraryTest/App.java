df0f7cff29bbc5abed9325788a3bb6b491287443
Crystal Cox <coxcs1@etsu.edu>
2017-08-01 10:12:30 -0400

    Add files via upload

---
+++ src/12FactorGoLibrary/ci/LibraryTest/src/main/java/com/LibraryTest/LibraryTest/App.java
@@ -0,0 +1,13 @@
+package com.LibraryTest.LibraryTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
