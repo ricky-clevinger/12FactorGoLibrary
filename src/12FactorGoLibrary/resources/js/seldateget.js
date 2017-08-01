6dca843f86fc0e97d5743649cf911098c47bc60e
Jonathan Quilliams <joqu16@gmail.com>
2017-08-01 12:36:28 -0400

    Updated seldateget.js getMonth() method

--- src/12FactorGoLibrary/resources/js/seldateget.js
+++ src/12FactorGoLibrary/resources/js/seldateget.js
@@ -1,6 +1,7 @@
-n = new Date();
-y = n.getFullYear();
-m = n.getMonth();
-d = n.getDate();
+var n = new Date();
+ y = n.getFullYear();
+ m = n.getMonth() + 1;
+ d = n.getDate();
 
-document.getElementById("selDate").value = y + "/" + m + "/" + d;
+n = y + "-" + m + "-" + d;
+document.getElementById("selDate").value = n;
