e6e3009bccccab41086160c076830ae0b5c13f5b
Jonathan Quilliams <joqu16@gmail.com>
2017-08-01 14:30:58 -0400

    Removed unused code from + member.go + book.go

--- src/12FactorGoLibrary/vendor/member/member.go
+++ src/12FactorGoLibrary/vendor/member/member.go
@@ -1,7 +1,7 @@
 package member
 
 //Author: C Neuhardt
-//Last Updated: 7/26/2017
+//Last Updated: 8/1/2017
 
 import (
 	"os"
@@ -10,7 +10,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//Gets the connection string "cgidevlib:Password1@tcp(cgiprojdevlibrary.cxyeb3wmov3g.us-east-1.rds.amazonaws.com:9871)/cgiprojdevlibrary"
+//Gets the connection string
 var connectionString = os.Getenv("LIBRARY")
 
 //Member Type
@@ -41,104 +41,6 @@ func GetMembers() []Member {
 
 	return members
 }
-
-//Return an array of IDs for each member in the db
-/*func GetIds() []int {
-	var memberIds []int
-	
-	db, err := sql.Open("mysql", connectionString)
-	checkErr(err)
-	defer db.Close()
-	
-	memberIdRows, err := db.Query("SELECT member_id FROM member")
-	checkErr(err)
-	for memberIdRows.Next() {
-		var MemberId int
-		err = memberIdRows.Scan(&MemberId)
-		checkErr(err)
-		memberIds = append(memberIds, MemberId)
-	}
-
-	return memberIds
-}
-
-//Return an array of First Names for each member in the db
-func GetFNames() []string {
-	var memberFNames []string
-	
-	db, err := sql.Open("mysql", connectionString)
-	checkErr(err)
-	defer db.Close()
-	
-	memberIdRows, err := db.Query("SELECT member_fname FROM member")
-	checkErr(err)
-	for memberIdRows.Next() {
-		var Member_FName string
-		err = memberIdRows.Scan(&Member_FName)
-		checkErr(err)
-		memberFNames = append(memberFNames, Member_FName)
-	}
-
-	return memberFNames
-}
-
-//Return an array of Last Names for each member in the db
-func GetLNames() []string {
-	var memberLNames []string
-	
-	db, err := sql.Open("mysql", connectionString)
-	checkErr(err)
-	defer db.Close()
-	
-	memberIdRows, err := db.Query("SELECT member_lname FROM member")
-	checkErr(err)
-	for memberIdRows.Next() {
-		var Member_LName string
-		err = memberIdRows.Scan(&Member_LName)
-		checkErr(err)
-		memberLNames = append(memberLNames, Member_LName)
-	}
-
-	return memberLNames
-}
-
-//Return First Name for a member using member_id
-func GetFNameById(memberId int) string {
-	db, err := sql.Open("mysql", connectionString)
-	checkErr(err)
-	defer db.Close()
-
-	queryString := fmt.Sprintf("SELECT member_fname FROM member WHERE member_id = %d", memberId)
-	memberRows, err := db.Query(queryString)
-	checkErr(err)
-	for memberRows.Next() {
-		var Member_FName string
-		err = memberRows.Scan(&Member_FName)
-		checkErr(err)
-		return Member_FName
-	}
-
-	return ""
-}
-
-//Return Last Name for a member using member_id
-func GetLNameById(memberId int) string {
-	db, err := sql.Open("mysql", connectionString)
-	checkErr(err)
-	defer db.Close()
-
-	queryString := fmt.Sprintf("SELECT member_lname FROM member WHERE member_id = %d", memberId)
-	memberRows, err := db.Query(queryString)
-	checkErr(err)
-	for memberRows.Next() {
-		var Member_LName string
-		err = memberRows.Scan(&Member_LName)
-		checkErr(err)
-		return Member_LName
-	}
-
-	return ""
-}*/
 
 //Checks for errors
 func checkErr(err error) {
